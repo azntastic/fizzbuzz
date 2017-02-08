12c4478c1583fbbf1fb7bf83a44400cfa21f9d9a
Ryan <Ryanchu.ws@gmail.com>
2017-02-08 14:17:21 +0000

    DONE

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(num)
-'fizz'
+  case
+  when num % 15 == 0 then 'fizzbuzz'
+  when num % 3 == 0 then 'fizz'
+  when num % 5 == 0 then 'buzz'
+  else
+    num
+end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,16 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
   it 'returns "fizz" when divisible by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when divisble by 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
